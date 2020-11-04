8c33c99460c75b3ffc12e06ace235ce5fc67981f
Bart Piasek <bartpiasek@icloud.com>
2020-11-04 20:20:16 +0100

    refactoring

--- monster-killer/assets/scripts/app.js
+++ monster-killer/assets/scripts/app.js
@@ -3,7 +3,17 @@ const STRONG_ATTACK_VALUE = 20;
 const MONSTER_ATTACK_VALUE = 17;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const MODE_ATTACK = 'ATTACK'; //MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; //MODE_ATTACK = 1 
+
+const enteredValue = prompt('Maximum life: ', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNan(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -44,9 +54,9 @@ function endRound() {
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode ==='ATTACK') {
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE;
-    } else if (mode === 'STRONG_ATTACK') {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
     const damage = dealMonsterDamage(maxDamage);
@@ -55,7 +65,7 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-    attackMonster('ATTACK');
+    attackMonster(MODE_ATTACK);
 
     // const damage = dealMonsterDamage(ATTACK_VALUE);
     // currentMonsterHealth -= damage;
@@ -71,7 +81,7 @@ function attackHandler() {
 }
 
 function strongAttackHandler() {
-    attackMonster('STRONG_ATTACK');
+    attackMonster(MODE_STRONG_ATTACK);
     // const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
     // currentMonsterHealth -= damage;
     // const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
